53de6f915eac7dc5cfaafa64b2ddae4de1fb3b10
jack <jslam@oulund.dk>
2024-03-14 11:39:31 +0100

    remove card from app

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,7 @@ import MainLayout from "./pages/MainLayout"
 function App() {
   return (
     <MainLayout>
-      <H1 text="Hello World!" />
-      <ServiceCard
-        service={{
-          name: "Service",
-          estimatedTime: 3600,
-          estimatedPrice: 2000,
-          imageUrl: "https://via.placeholder.com/150",
-        }}
-      />
+      <H1 text="Hello World!" /> 
     </MainLayout>
   )
 }
