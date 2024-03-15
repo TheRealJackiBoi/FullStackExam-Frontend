41697407d3c6b84a23b8cfa85bb3237f9f716e71
Julius Lassen <julius5517@gmail.com>
2024-03-15 22:07:23 +0100

    fixed names

--- src/graphql/booking/bookingMutations.ts
+++ src/graphql/booking/bookingMutations.ts
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client"
 
-export const createBooking = gql`
+export const CREATE_BOOKING = gql`
   mutation Mutation($startTime: String!, $endTime: String!, $status: Status!, $device: String!, $cost: Float!, $serviceId: ID!, $token: String!) {
     createBooking(startTime: $startTime, endTime: $endTime, status: $status, device: $device, cost: $cost, serviceId: $serviceId, token: $token) {
       _id
@@ -20,7 +20,7 @@ export const createBooking = gql`
   }
 `
 
-export const updateBooking = gql`
+export const UPDATE_BOOKING_BY_ID = gql`
   mutation Mutation($id: ID!, $device: String!, $cost: Float!, $serviceId: ID!, $token: String!, $status: Status) {
     updateBooking(_id: $id, device: $device, cost: $cost, serviceId: $serviceId, token: $token, status: $status) {
       _id
@@ -40,7 +40,7 @@ export const updateBooking = gql`
   }
 `
 
-export const deleteBooking = gql`
+export const DELETE_BOOKING = gql`
   mutation Mutation($id: ID!, $token: String!) {
     deleteBooking(_id: $id, token: $token) {
       _id
