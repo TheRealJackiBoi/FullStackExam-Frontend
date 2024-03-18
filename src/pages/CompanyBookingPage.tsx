c1604583d660d826a8712466fea671b5f8544fd8
Julius Lassen <julius5517@gmail.com>
2024-03-18 17:38:32 +0100

    styling

--- src/pages/CompanyBookingPage.tsx
+++ src/pages/CompanyBookingPage.tsx
@@ -2,7 +2,7 @@ import { H1, H3 } from "@/components/Typography"
 import { GET_COMPANY_BY_ID } from "@/graphql/company/companyQueries"
 import { Company } from "@/types/companyTypes"
 import { useQuery } from "@apollo/client"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { Calendar } from "@/components/ui/calendar"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
@@ -18,7 +18,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 
 interface CompanyData {
   company: Company
@@ -65,42 +65,71 @@ function BookingRender({ company }: CompanyData) {
 
   return (
     <>
+      <div className="absolute ">
+        <Link
+          className={buttonVariants({ variant: "destructive" })}
+          to={`/company/${company._id}`}
+        >
+          <span className="text-sm md:text-base">Tilbage</span>
+        </Link>
+      </div>
       <div className="m-4 flex justify-center">
         <H1 text={"Book en service hos " + company.name}></H1>
       </div>
-      <Card>
-        <CardContent className="flex space-x-6 items-center ">
-          <div>
-            <H3 text="vælg indleveringsdato"></H3>
-            <Calendar mode="single" selected={date} onSelect={setDate} />
-          </div>
-          <div className="w-6/12 mx-auto mt-10 pt-6">
-            <Form {...forminput}>
-              <form
-                onSubmit={forminput.handleSubmit(onSubmit)}
-                className="flex flex-col"
-              >
-                <FormField
-                  control={forminput.control}
-                  name="enhed"
-                  render={({ field }) => (
-                    <FormItem className="flex items-center mb-4">
-                      <FormLabel className="mr-2">Enhed</FormLabel>
-                      <FormControl>
-                        <Input className="max-w-md" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <div className="text-center scale-125">
-                  <Button type="submit">Bestil</Button>
-                </div>
-              </form>
-            </Form>
-          </div>
-        </CardContent>
-      </Card>
+      <div className="flex flex-col md:flex-row justify-center space-y-6 md:space-y-0 md:space-x-10">
+        <Card className="p-4">
+          <CardContent>
+            <div className="text-center">
+              <H3 text="Vælg indleveringsdato"></H3>
+              <Calendar mode="single" selected={date} onSelect={setDate} />
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="p-4">
+          <CardContent>
+            <div className="w-full md:w-72 col">
+              <Form {...forminput}>
+                <form onSubmit={forminput.handleSubmit(onSubmit)}>
+                  <FormField
+                    control={forminput.control}
+                    name="enhed"
+                    render={({ field }) => (
+                      <FormItem className="flex items-center mb-4">
+                        <FormLabel className="mr-2">Enhed</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField 
+                    name="service"
+                    render={({ field }) => (
+                        <FormItem className="">
+                            <FormLabel className="mr-2">Service</FormLabel>
+                            <FormControl>
+                                <Input {...field } />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                  />
+                    
+                  <div className="text-center scale-125">
+                    <Button
+                      type="submit"
+                      className=" bg-blue-500 text-white hover:bg-blue-300"
+                    >
+                      Bestil
+                    </Button>
+                  </div>
+                </form>
+              </Form>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </>
   )
 }
